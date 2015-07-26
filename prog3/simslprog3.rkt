be4e2b65d7d6afa8eab1b59e9a5ea0ddc522d094
simsl <logan.sims94@gmail.com>
2015-07-26 02:06:12 -0700

    need to comment prog 3

--- prog3/simslprog3.rkt
+++ prog3/simslprog3.rkt
@@ -2,6 +2,24 @@
 (require racket/include)
 (include "Program-3-data.rkt")
 
+(define node% 
+  (class object%
+    (init r)               
+    (define room r)
+    (define parent '())
+    (define direction '())
+    (define/public (get-room)
+    room)
+    (define/public (get-parent)
+    parent)
+    (define/public (get-direction)
+    direction)
+    (define/public (set-parent p)
+    (set! parent p)) 
+    (define/public (set-direction p)
+    (set! direction p))    
+    (super-new)))
+
 ;global for robbie's location
 (define loc '())
 
@@ -35,10 +53,10 @@
   (lambda ()
     (cond ((regexp-match "-stairs" (symbol->string loc))
            (display "Robbie is on the ")
-           (displayln (symbol->string (car loc))))
-          ((regexp-match "stairs" (symbol->string loc))
+           (displayln (symbol->string loc)))
+          ((regexp-match "stairs-" (symbol->string loc))
            (display "Robbie is in the ")
-           (displayln (symbol->string (car loc))))
+           (displayln (symbol->string loc)))
           (else  
            (cond((down-stairs? loc)
                  (display "Robbie is downstairs in the ")
@@ -46,7 +64,7 @@
                  )
                 (else
                 (display "Robbie is upstairs in the ")
-                 (displayln (symbol->string (car loc)))
+                 (displayln (symbol->string loc))
                  ))))))
 
 ;determines if a location is down-stairs.
@@ -70,49 +88,66 @@
 
 (define move
   (lambda (direction)
+    (define moved #f)
     (define dest '(Ouch! Robbie hit the wall))
     (cond ((not (equal? (look direction loc) 'NIL))
            (for ((room rooms))
-             (cond ((equal? (car room) (look direction loc))
+             (cond ((and (equal? (car room) (look direction loc)) (not moved))
                     (set-robbie-location (car room))
-                    (set! dest (where)))))))
+                    (set! dest (where))
+                    (set! moved #t))))))
     dest))
 
+; Uses BFS to find shortest path from src to dest
+;
 (define traverse
   (lambda (src dest)
-    (displayln "traversing..")
+    (define root (new node% (r src)))
+    (define currNode root)
+    (define newNode '())    
+    (define v (list src))
+
+    (define Q '())
+    (define solution '())
+    (set! Q (append (list root) Q))
     
-    ))
+    (let loop ()
+      (cond((not (empty? Q))
+            (set! currNode (car Q))
+            (set! Q (cdr Q))
+            (for ((next-location (choices (send currNode get-room))))
+              (cond ((not(member (list-ref next-location 1) v))
+                     (set! v (append v (cdr next-location)))
+                     (set! newNode (new node% (r (list-ref next-location 1))))
+                     (send newNode set-parent currNode)
+                     (send newNode set-direction (list-ref next-location 0))
+                     (set! Q (append (list newNode) Q))
+                     (cond ((equal? (list-ref next-location 1) dest)
+                            (set! solution newNode)
+                            (set! Q '()))))))
+            (loop))))
+    (print-path solution)))
+
+;Given a node the contains the dest, recursivly
+;travels up parents to print the path from src to dest.
+(define print-path
+  (lambda (node)
+  (cond ((not(equal? (send node get-parent) '()))
+         (print-path (send node get-parent))))
+  (cond ((not (equal? (send node get-direction) '()))
+         (display "Go ")
+         (display (send node get-direction))
+         (display " to ")
+         (displayln (send node get-room))))))
 
 
+;TESTING;;;;;;;;;;;;;;;;;;;
 
 (set-robbie-location 'kitchen)
 (traverse loc 'library)
 
-
-
-
-
-
-
-
-
-
-
-;TEST
-;(set-robbie-location 'kitchen)
-
-;(displayln ">(where)")
-;(where)
-;(displayln ">(choices loc)")
-;(choices loc)
-;(displayln ">(move 'north)")
-;(move 'north)
-;(displayln ">(move 'west)")
-;(move 'west)
-;(displayln ">(choices loc)")
-;(choices loc)
-;(displayln ">(move 'south)")
-;(move 'south)
-;(displayln "(where)")
-;(where)
+(move 'south)
+(move 'west)
+(move 'west)
+(move 'north)
+(move 'north)
